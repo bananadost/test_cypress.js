dc6a9b1240ef0b9c221810da1a5f43599114c750
Olga <122206809+bananadost@users.noreply.github.com>
2023-07-09 00:00:46 +0300

    Add files via upload

--- cypress/e2e/orderkd.cy.js
+++ cypress/e2e/orderkd.cy.js
@@ -2,10 +2,18 @@ describe('Оформление заказа', function () {
     
     it('Оформление заказа', function () {
         cy.visit('https://reddragon-spb.ru/');
-        cy.get('[href="/koreyskaya-kukhnya/"] > .item-tcat__image > .item-tcat__img').wait(1000).click();
-        cy.get(':nth-child(5) > .product-item__inner > .product-item__offers > .purchase > .product-item__buttons > .btn').wait(1000).click();
-        cy.get(':nth-child(6) > .product-item__inner > .product-item__offers > .purchase > .product-item__buttons > .btn').wait(1000).click();
-        cy.get(':nth-child(9) > .product-item__inner > .product-item__offers > .purchase > .product-item__buttons > .btn').wait(1000).click();
+        cy.get(
+            '[href="/koreyskaya-kukhnya/"] > .item-tcat__image > .item-tcat__img'
+            ).wait(1000).click();
+        cy.get(
+            ':nth-child(5) > .product-item__inner > .product-item__offers > .purchase > .product-item__buttons > .btn'
+            ).wait(1000).click();
+        cy.get(
+            ':nth-child(6) > .product-item__inner > .product-item__offers > .purchase > .product-item__buttons > .btn'
+            ).wait(1000).click();
+        cy.get(
+            ':nth-child(9) > .product-item__inner > .product-item__offers > .purchase > .product-item__buttons > .btn'
+            ).wait(1000).click();
         cy.get('.icon-cart').wait(1000).click();
         cy.on('uncaught:exception', (err, runnable) => {return false});
         cy.contains('Оформление заказа');
@@ -13,9 +21,13 @@ describe('Оформление заказа', function () {
         cy.get('.wa-field-lastname > .wa-value > input').wait(1000).type('Иванов');
         cy.get('.wa-field-phone > .wa-value > p > input').wait(1000).type('+79111111111');
         cy.get('.wa-field-email > .wa-value > p > input').wait(1000).type('test@ya.ru');
-        cy.get('#checkout-contact-form > .wa-form > .wa-field-address > .wa-value > p > .wa-field-address-street > input').wait(1000).type('тест');
+        cy.get(
+            '#checkout-contact-form > .wa-form > .wa-field-address > .wa-value > p > .wa-field-address-street > input'
+            ).wait(1000).type('тест');
         cy.get('.cartOS__terms > input').wait(1000).click();
-        cy.get('.cartOS__sidebar-inner > .cartOS__info > .cartOS__infoBl > .cartOS__buttons > .cartOS__button').wait(1000).click();
+        cy.get(
+            '.cartOS__sidebar-inner > .cartOS__info > .cartOS__infoBl > .cartOS__buttons > .cartOS__button'
+            ).wait(1000).click();
         cy.wait(5000);
         cy.contains('Ваш заказ успешно оформлен');
     })
